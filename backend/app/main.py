0b09196d28167f9d705545c5fb417c82f3eec63b
Sivakumar10036 <rsiva19022005@gmail.com>
2026-08-22 21:48:51 +0530

    Updated vercel link

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,7 @@ app = FastAPI(
 allowed_origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://smart-ai-resume-screener.vercel.app"
+    "https://smart-resume-ai-screener.vercel.app"
 ]
 
 
